c4d4455ed517260af9b3fa8d3380537ae7ab4ced
Александр <apamic1995@gmail.com>
2020-07-23 15:55:24 +0300

    from method to class

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, by_rubric, BbCreateView, add_and_save
+from .views import BbIndexView, BbByRubricView, BbDetailView, BbAddView, BbEditView, BbDeleteView
 
 urlpatterns = [
-    path('add/', add_and_save, name='add'),
-    path('', index, name='index'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add/', BbAddView.as_view(), name='add'),
+    path('update/<int:pk>', BbEditView.as_view(), name='update'),
+    path('delete/<int:pk>', BbDeleteView.as_view(), name='delete'),
+    path('', BbIndexView.as_view(), name='index'),
+    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
+    path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
     #path('add/', BbCreateView.as_view(), name='add'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,46 +1,80 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Bb, Rubric
-from django.views.generic.edit import CreateView
+from django.views.generic import FormView, TemplateView, DetailView, ListView, UpdateView, DeleteView
 from .forms import BbForm
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse
 
 
-def index(request):
-    bbs = Bb.objects.all()
-    rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics': rubrics}
-    return render(request, 'bboard/index.html', context)
+class BbIndexView(TemplateView):
+    template_name = 'bboard/index.html'
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
-    return render(request, 'bboard/by_rubric.html', context)
-
-class BbCreateView(CreateView):
-    template_name = 'bboard/create.html'
-    form_class = BbForm
-    success_url = reverse_lazy('index')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['bbs'] = Bb.objects.all()
         context['rubrics'] = Rubric.objects.all()
         return context
 
+class BbDetailView(DetailView):
+    model = Bb
 
-def add_and_save(request):
-    if request.method == 'POST':
-        bbf = BbForm(request.POST)
-        if bbf.is_valid():
-            bbf.save()
-            return HttpResponseRedirect(reverse('by_rubric',
-                            kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
-        else:
-            context = {'form': bbf}
-            return render(request, 'bboard/create.html', context)
-    else:
-        bbf = BbForm()
-        context = {'form': bbf}
-        return render(request, 'bboard/create.html', context)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+class BbByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=self.kwargs['rubric_id'])
+        return context
+
+class BbAddView(FormView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    initial = {'price': 0.0}
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('by_rubric',
+                       kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
+
+class BbEditView(UpdateView):
+    template_name = 'bboard/bb_form.html'
+    model = Bb
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+class BbDeleteView(DeleteView):
+    template_name = 'bboard/bb_confirm_delete.html'
+    model = Bb
+    success_url = '/bboard/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
